2a67bff2b4055f135f03a5d04ee8af17ee443e39
juanpablogt <juapabgonzalez@utp.edu.co>
2026-09-06 11:57:42 -0500

    Practica 7

--- practica_7/ec2.tf
+++ practica_7/ec2.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-0332d564d76dbd8d6"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_virginia.id
+  key_name      = data.aws_key_pair.key.key_name
 }
