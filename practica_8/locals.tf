618628a6aa33e0ee35a71af71ce26a8cc7be4c8a
juanpablogt <54081160+juanpablogt@users.noreply.github.com>
2026-09-24 13:16:11 -0500

    S3

--- practica_8/locals.tf
+++ practica_8/locals.tf
@@ -1,3 +1,13 @@
 locals {
-  sufix = "${var.project}-${var.region}-${var.env}"
+  sufix = "${var.tags["project"]}-${var.tags["region"]}-${var.tags["env"]}"
+}
+
+resource "random_string" "random" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+locals {
+  s3-sufix = lower(replace("${local.sufix}-${random_string.random.result}", "_", "-"))
 }
--- practica_8/providers.tf
+++ practica_8/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.61.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
   required_version = "~> 1.15.0"
 }
