4cdd00eaf4e0f4b99d80f15237fe1d7fdaa0f794
juanpablogt <54081160+juanpablogt@users.noreply.github.com>
2026-09-24 12:49:17 -0500

    terreform tags vpcs

--- practica_8/vpc.tf
+++ practica_8/vpc.tf
@@ -8,6 +8,7 @@ resource "aws_vpc" "VPC_virginia" {
 resource "aws_subnet" "public_subnet_virginia" {
   vpc_id                  = aws_vpc.VPC_virginia.id
   cidr_block              = var.subnets[0]
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet Virginia-${local.sufix}"
